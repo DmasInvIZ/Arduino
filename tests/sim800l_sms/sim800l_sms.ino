e011c1a5d5cdf8bb48e623658053ece203d2d233
DmasInvIZ <inviz1806@gmail.com>
2023-02-18 22:43:27 +0300

    fixes

--- tests/sim800l_sms/sim800l_sms.ino
+++ tests/sim800l_sms/sim800l_sms.ino
@@ -13,6 +13,8 @@ String textSms,numberSms;
 uint8_t index1;
 uint8_t LED2=13; // use what you need 
 bool error;
+String on = "TURNON";
+String off = "TURNOFF";
 
 void setup() {
   Serial.begin(9600);               // Скорость обмена данными с компьютером
@@ -27,19 +29,20 @@ void setup() {
 }
 void loop() {
   Serial.println("scan");
+  Serial.println(textSms);
   textSms=Sim800l.readSms(1);                                //read the first sms
     if (textSms.indexOf("OK")!=-1) {                         //first we need to know if the messege is correct. NOT an ERROR
       numberSms=Sim800l.getNumberSms(1);                     // Here you have the number
       Serial.println(numberSms); 
       textSms.toUpperCase();                                 // set all char to mayus ;)
-      if (textSms.indexOf("TURNON")!=-1){
+      if (textSms.indexOf(on)!=-1) {
           Serial.println("LED TURN ON");
-          digitalWrite(LED2,1);
+          digitalWrite(LED2, 1);
           sending = Sim800l.sendSms("+375295689321", "ON");
       }
-      else if (textSms.indexOf("TURNOFF")!=-1){
+      else if (textSms.indexOf(off)!=-1) {
           Serial.println("LED TURN OFF");
-          digitalWrite(LED2,0);
+          digitalWrite(LED2, 0);
           sending = Sim800l.sendSms("+375295689321", "OFF");
       }
       else{
