a3aa68e6952e55a3edead2163f6f3382d1b16a69
Dmitry <inviz1806@gmail.com>
2024-02-26 11:47:05 +0300

    добавлены новые проекты

--- protect with SMS/protect-sms/protect-sms.ino
+++ protect with SMS/protect-sms/protect-sms.ino
@@ -4,13 +4,13 @@
   Под мойкой в кухне, под фильтром в кухне, под ванной, 2 под стиральной машиной, 1 в стояке с коммуникациями
   При срабатывании любого датчика включается сигнализация на 20 сек.
   После того как обнаружена протечка включается пищалка, отсылается смс на указанные номера 
-  и мигает соответсвутющая лампочка.
+  информация с зоной протечки выводится на дисплей
   Работа алгоритма прекращается до перезагрузки МК.
 
   - Подключение датчиков: пины 2-7
   - 7, 8 пины для подключения sim800l
   - 11 пин - пищалка
-  - A0-A5 - для светодиодов
+  - A4, A5 - дисплей
 
   В следующих версиях прошивки:
   работы программы не останавливается после обнаружения протечки
@@ -19,68 +19,51 @@
 #include <SoftwareSerial.h>
 #include <Sim800l.h>
 #include <EncButton.h>
-//#include "GyverButton.h"
+#include <LiquidCrystal_I2C.h>
 
-#define buzz_pin 11                                           // пин сигналки   
-#define BTN_PIN 10                                            // кнопка
+#define buzz_pin 13                                           // пин сигналки   
+#define BTN_PIN 3                                             // кнопка теста
 #define SMStext "Alarm! Zone: "                               // текст смс на телефон
 #define tel_number "+375295689321"                            // номер телефона для отправки
 #define tel_number2 "+375257769952"                           // 2 номер телефона для отправки
 #define alarm_sec 2                                           // пищим столько раз (секунд)
 #define test_alarm_sec 2                                      // пищим столько раз (секунд) для теста сигнализации
 
-#define led_kitchen A0
-#define led_water_filter A1
-#define led_bath A2
-#define led_washer A3
-#define led_sewerage A4
-
-//GButton butt1(BTN_PIN);
-EncButton<EB_TICK, BTN_PIN> enc;  ///////////////
+EncButton<EB_TICK, BTN_PIN> enc;
 
 Sim800l Sim800l;
-SoftwareSerial SIM800(8, 9);                   // 8 - RX Arduino (TX SIM800L), 9 - TX Arduino (RX SIM800L)
+SoftwareSerial SIM800(8, 9);                                  // 8 - RX Arduino (TX SIM800L), 9 - TX Arduino (RX SIM800L)
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);                           // инициализируем переменную lcd
 
 
-//имена зон конотроля
-int kitchen = 2;
-int water_filter = 3;
-int bath = 4;
-int washer1 = 5;
-int washer2 = 6;
-int sewerage = 7;
+// имена зон конотроля
+#define kitchen         2
+#define water_filter    3
+#define bath            4
+#define washer1         5
+#define washer2         6
+#define sewerage        7
 
-void alarm_test() {
-  Serial.println("alarm test");
-  int test_counter = test_alarm_sec;
-  while (test_counter > 0) {
-    digitalWrite(led_kitchen, HIGH);
-    digitalWrite(led_water_filter, HIGH);
-    digitalWrite(led_bath, HIGH);
-    digitalWrite(led_washer, HIGH);
-    digitalWrite(led_sewerage, HIGH);
+
+// функция пищания сигнализации (добавить время работы сигнала)
+void alarming(int blink_time) {
+  while (blink_time > 0) {
     digitalWrite(buzz_pin, HIGH);
     delay(500);
-    digitalWrite(led_kitchen, LOW);
-    digitalWrite(led_water_filter, LOW);
-    digitalWrite(led_bath, LOW);
-    digitalWrite(led_washer, LOW);
-    digitalWrite(led_sewerage, LOW);
     digitalWrite(buzz_pin, LOW);
     delay(500);
-    test_counter -= 1;
+    blink_time -= 1;
   }
-  Sim800l.sendSms(tel_number, "Test SMS, module OK");
-  Serial.println("send sms");
 }
 
-void led_blink(int zone_led) {
-  while (true) {
-    digitalWrite(zone_led, LOW);
-    delay(300);
-    digitalWrite(zone_led, HIGH);
-    delay(300);
-  }
+// тест 
+void alarm_test() {
+  Serial.println("alarm test");
+  //////////////////////////////////////////////
+  alarming(3);
+  Sim800l.sendSms(tel_number, "Test SMS, module OK");
+  Serial.println("send sms");
 }
 
 void SendSMS(String SMStext_zone) {
@@ -89,21 +72,8 @@ void SendSMS(String SMStext_zone) {
   Serial.println("alarm message sent");
 }
 
-void alarm_on(int zone_led, String zone) {
-  Serial.println("Alarm!");
-  Serial.println(zone_led);
-  digitalWrite(zone_led, HIGH);
-  int counter = alarm_sec;                                 // пищим столько раз (секунд)
-  while (counter > 0) {
-    digitalWrite(buzz_pin, HIGH);
-    delay(500);
-    digitalWrite(buzz_pin, LOW);
-    delay(500);
-    counter -= 1;
-  }
-  SendSMS(zone);
-  Serial.println("led blink");
-  led_blink(zone_led);                                    // включаем светодиод на панели для наглядности
+void alarm_on(zone, text) {
+  
 }
 
 void setup() {
@@ -113,11 +83,6 @@ void setup() {
   Sim800l.begin();                            // Инициализация модема
   SIM800.println("AT");
   pinMode(buzz_pin, OUTPUT);
-  pinMode(led_kitchen, OUTPUT);
-  pinMode(led_water_filter, OUTPUT);
-  pinMode(led_bath, OUTPUT);
-  pinMode(led_washer, OUTPUT);
-  pinMode(led_sewerage, OUTPUT);
   pinMode(BTN_PIN, INPUT_PULLUP);             // пин кнопки с внутренней подтяжкой
   delay(1000);
   Serial.println("Ready.");
@@ -125,19 +90,19 @@ void setup() {
 
 void loop() {
   if (digitalRead(kitchen) == HIGH) {
-    alarm_on(led_kitchen, "Alarm! Zone: kitchen");
+    alarm_on(kitchen, "Alarm! Zone: kitchen");
   }
   if (digitalRead(water_filter) == HIGH) {
-    alarm_on(led_water_filter, "Alarm! Zone: kitchen, water filter");
+    alarm_on(water_filter, "Alarm! Zone: kitchen, water filter");
   }
   if (digitalRead(bath) == HIGH) {
-    alarm_on(led_bath, "Alarm! Zone: bathroom, under bath");
+    alarm_on(bath, "Alarm! Zone: bathroom, under bath");
   }
   if (digitalRead(washer1) == HIGH || digitalRead(washer2) == HIGH) {
-    alarm_on(led_washer, "Alarm! Zone: washing machine");
+    alarm_on(washer, "Alarm! Zone: washing machine");
   }
   if (digitalRead(sewerage) == HIGH) {
-    alarm_on(led_sewerage, "Alarm! Zone: sewerage, bathroom, behind the toilet");
+    alarm_on(sewerage, "Alarm! Zone: sewerage, bathroom, behind the toilet");
   }
   //butt1.tick();
   enc.tick();
@@ -152,3 +117,32 @@ void loop() {
   delay(30);
   //Serial.println("Check");
 }
+
+
+//-----------------------------------------------------------------------------------------------------
+//надо проверить код
+//
+//#include <LiquidCrystal.h>
+//
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Объект для работы с LCD дисплеем
+//const int analogPin = A0; // Пин, к которому подключено напряжение
+//const int maxVoltage = 5; // Максимальное напряжение питания
+//
+//void setup() {
+//  lcd.begin(16, 2); // Инициализация LCD дисплея
+//}
+//
+//void loop() {
+//  int sensorValue = analogRead(analogPin); // Считываем значение с аналогового пина
+//  float voltage = sensorValue * maxVoltage / 1023.0; // Преобразуем значение в напряжение
+//  int batteryPercent = map(voltage, 0, maxVoltage, 0, 100); // Конвертируем напряжение в процент заряда
+//
+//  lcd.clear(); // Очищаем экран перед выводом новых данных
+//  lcd.setCursor(0, 0); // Устанавливаем курсор на первую строку и первый столбец
+//  lcd.print("Battery Level:"); // Выводим текст на LCD дисплей
+//  lcd.setCursor(0, 1); // Устанавливаем курсор на вторую строку и первый столбец
+//  lcd.print(batteryPercent); // Выводим процент заряда
+//  lcd.print("%"); // Выводим знак процента
+//
+//  delay(1000); // Ждем 1 секунду перед повторным чтением
+//}
--- tests/sim800l_sms test/test-sms-sim800l/test-sms-sim800l.ino
+++ tests/sim800l_sms test/test-sms-sim800l/test-sms-sim800l.ino
@@ -26,20 +26,26 @@ void setup() {
   SIM800.begin(9600);               // Скорость обмена данными с модемом
   Sim800l.begin();                                                       // инициализируем библиотеку
   SIM800.println("AT");
-  sending = Sim800l.sendSms(tel_number, sms_text);   // шлем сообщение
-  Serial.println(sending);
-  Serial.println("Message sent");
+  //sending = Sim800l.sendSms(tel_number, sms_text);   // шлем сообщение
+  //Serial.println(sending);
+  //Serial.println("Message sent");
 }
 
 void loop() {
-  textSms=Sim800l.readSms(1);                                            // читаем смс
-  Serial.println(textSms);                                               // пишем текст в консоль
-  if (textSms.indexOf("OK")!=-1) {                                       // убеждаемся что пришел ответ от модуля о выполнении команды
-    numberSms=Sim800l.getNumberSms(1);                                   // получаем номер абонента приславшего смс
-    Serial.println(numberSms);                                           // пишем в консоль номер абонента приславшего смс
-    Sim800l.sendSms(numberSms, textSms);                                 // когда получаем смс отправляем ответ
-    Serial.println("Answer sent");
-    Sim800l.delAllSms();                                                 // удаляет все сообщения
-  }
-  Serial.println("iter");                                                // выводит при каждой итерации цикла loop
+  //textSms=Sim800l.readSms(1);                                            // читаем смс
+  //Serial.println(textSms);                                               // пишем текст в консоль
+
+  if (SIM800.available())           // Ожидаем прихода данных (ответа) от модема...
+  Serial.write(SIM800.read());    // ...и выводим их в Serial
+  if (Serial.available())           // Ожидаем команды по Serial...
+  SIM800.write(Serial.read());    // ...и отправляем полученную команду модему
+  
+//  if (textSms.indexOf("OK")!=-1) {                                       // убеждаемся что пришел ответ от модуля о выполнении команды
+//    numberSms=Sim800l.getNumberSms(1);                                   // получаем номер абонента приславшего смс
+//    Serial.println(numberSms);                                           // пишем в консоль номер абонента приславшего смс
+//    Sim800l.sendSms(numberSms, textSms);                                 // когда получаем смс отправляем ответ
+//    Serial.println("Answer sent");
+//    Sim800l.delAllSms();                                                 // удаляет все сообщения
+//  }
+//  Serial.println("iter");                                                // выводит при каждой итерации цикла loop
 }
